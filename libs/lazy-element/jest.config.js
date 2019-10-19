080060510fe5fe592d8fecfc7b3d00187168cdf5
Jimmy Liu <hero63418@gmail.com>
2019-10-20 02:08:23 +0800

    add  collectCoverageFrom

--- libs/lazy-element/jest.config.js
+++ libs/lazy-element/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'lazy-element',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/lazy-element',
+  collectCoverageFrom: ['./src/lib/**/*.ts'],
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
--- libs/lazy-module/jest.config.js
+++ libs/lazy-module/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'lazy-module',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/lazy-module',
+  collectCoverageFrom: ['./src/lib/**/*.ts'],
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
