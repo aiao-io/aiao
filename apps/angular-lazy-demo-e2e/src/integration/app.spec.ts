23ba48154467f56e851017aa30e8a25e0a9d805a
Jimmy Liu <hero63418@gmail.com>
2019-10-19 23:45:40 +0800

    update

--- apps/angular-lazy-demo-e2e/src/integration/app.spec.ts
+++ apps/angular-lazy-demo-e2e/src/integration/app.spec.ts
@@ -4,6 +4,6 @@ describe('angular-lazy-demo', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to angular-lazy-demo!');
+    getGreeting().contains('root');
   });
 });
