73c55a003e90282d2c6bc9294122da989fb34581
Jimmy Liu <hero63418@gmail.com>
2019-10-22 18:12:15 +0800

    update

--- apps/aiao.io/src/app/not-found/not-found.module.ts
+++ apps/aiao.io/src/app/not-found/not-found.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
