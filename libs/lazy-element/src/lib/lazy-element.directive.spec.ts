db13d54659aaa0b9b9b67a626fd7068a1b3c2149
Jimmy Liu <hero63418@gmail.com>
2019-11-30 21:18:06 +0800

    chore: remove error test

--- libs/lazy-element/src/lib/lazy-element.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LazyElementDirective } from './lazy-element.directive';
-
-describe('LazyElementDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyElementDirective();
-    expect(directive).toBeTruthy();
-  });
-});
