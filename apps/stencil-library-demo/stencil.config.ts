6cc133df44e47a302e02d5e2a5c0503837280eca
Jimmy Liu <hero63418@gmail.com>
2019-12-02 17:32:06 +0800

    update

--- apps/stencil-library-demo/stencil.config.ts
+++ apps/stencil-library-demo/stencil.config.ts
@@ -1,3 +1,6 @@
+import { resolve } from 'path';
+import { cwd } from 'process';
+
 import { Config } from '@stencil/core';
 
 export const config: Config = {
@@ -12,6 +15,12 @@ export const config: Config = {
     },
     {
       type: 'dist-hydrate-script'
+    }
+  ],
+  copy: [
+    {
+      src: resolve(cwd(), 'node_modules/@ionic/core'),
+      dest: resolve(cwd(), 'apps/stencil-library-demo/vendors/ionic')
     }
   ],
   tsconfig: 'tsconfig.json'
--- libs/stencil/src/lib/builders/build/build.impl.ts
+++ libs/stencil/src/lib/builders/build/build.impl.ts
@@ -5,10 +5,8 @@ import { map, switchMap } from 'rxjs/operators';
 
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
-import { OutputTargetDist } from '@stencil/core/dist/declarations';
 
 import { copyAssets } from '../../util/copy-assets';
-import { requireConfigFile } from '../../util/load-ts-config-file';
 import { run } from '../../util/runner';
 
 export interface StencilBuilderOptions extends JsonObject {
@@ -30,60 +28,47 @@ function stencilBuild(options: StencilBuilderOptions, context: BuilderContext): 
   return from(readJson(join(workspaceRoot, 'angular.json'))).pipe(
     map(c => c.projects[project]),
     switchMap(projectConfig => {
-      const args = ['build'];
-      const configPath = join(workspaceRoot, config_path);
-      args.push(`--config ${configPath}`);
-
-      // args
+      const args = ['build', `--config ${join(workspaceRoot, config_path)}`];
       if (docs) {
         args.push('--docs');
       }
       if (stats) {
         args.push('--stats');
       }
-
-      const config = requireConfigFile(configPath);
-
-      const distOutputTarget: OutputTargetDist = config.outputTargets.find(d => d.type === 'dist') as OutputTargetDist;
-
-      const esmLoaderPath = distOutputTarget.esmLoaderPath;
-      const projectRoot = resolve(workspaceRoot, projectConfig.root);
-      const output = resolve(workspaceRoot, outputPath);
-
-      const projectPkg = readJSONSync(resolve(projectRoot, 'package.json'));
-      const files = projectPkg.files || [];
-      const assets = [];
-
-      files.forEach((file: string) => {
-        const fp = resolve(projectRoot, file);
-        const stat = statSync(fp);
-        const fileIsInProject = fp.includes(projectRoot);
-        const input = fileIsInProject ? projectRoot : workspaceRoot;
-        const filePath = fp.replace(input + '/', '');
-        if (stat.isDirectory()) {
-          assets.push({
-            glob: join(filePath, '**/*'),
-            input,
-            output
-          });
-        } else {
-          assets.push({
-            glob: filePath,
-            input,
-            output
-          });
+      return run('node_modules/.bin/stencil', args).then(() => {
+        if (!outputPath) {
+          return;
         }
+        const projectRoot = resolve(workspaceRoot, projectConfig.root);
+        const projectPkg = readJSONSync(resolve(projectRoot, 'package.json'));
+        const output = resolve(workspaceRoot, outputPath);
+        const assets = [];
+        projectPkg.files.forEach((file: string) => {
+          const fp = resolve(projectRoot, file);
+          const stat = statSync(fp);
+          const fileIsInProject = fp.includes(projectRoot);
+          const input = fileIsInProject ? projectRoot : workspaceRoot;
+          const filePath = fp.replace(input + '/', '');
+          if (stat.isDirectory()) {
+            assets.push({
+              glob: join(filePath, '**/*'),
+              input,
+              output
+            });
+          } else {
+            assets.push({
+              glob: filePath,
+              input,
+              output
+            });
+          }
+        });
+        try {
+          removeSync(output);
+        } catch {}
+        return copyAssets(assets, [''], output);
       });
-      if (esmLoaderPath.startsWith('../')) {
-      }
-      return run('node_modules/.bin/stencil', args)
-        .then(() => {
-          try {
-            removeSync(output);
-          } catch (error) {}
-          return copyAssets(assets, [''], output);
-        })
-        .then(() => ({ success: true }));
-    })
+    }),
+    map(d => ({ success: true }))
   );
 }
