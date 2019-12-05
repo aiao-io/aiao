329438c943c70bf9cce89c06b7fae1b512825d7f
Jimmy Liu <hero63418@gmail.com>
2019-12-05 17:58:03 +0800

    update

--- libs/lazy-module/src/lib/lazy-module-loader.spec.ts
+++ libs/lazy-module/src/lib/lazy-module-loader.spec.ts
@@ -1,11 +1,12 @@
 import { LazyModuleLoader, LazyRoutes, matcher } from '@aiao/lazy-module';
-import { ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type } from '@angular/core';
+import { ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, Compiler } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 describe('LazyModuleLoader', () => {
   let lazyModuleLoader: LazyModuleLoader;
   let lazymodules: LazyRoutes[];
   let aModule: FakeModuleFactory;
+  let compiler: Compiler;
 
   beforeEach(() => {
     aModule = new FakeModuleFactory('a-module');
@@ -15,6 +16,16 @@ describe('LazyModuleLoader', () => {
           name: 'AModule',
           loadChildren: () => Promise.resolve(aModule),
           matcher
+        },
+        {
+          name: 'BModule',
+          loadChildren: () => Promise.resolve(FakeCustomElementModule),
+          matcher
+        },
+        {
+          name: 'errModule',
+          loadChildren: () => Promise.resolve(FakeCustomElementModule),
+          matcher
         }
       ]
     ];
@@ -22,16 +33,87 @@ describe('LazyModuleLoader', () => {
       providers: [LazyModuleLoader]
     });
     lazyModuleLoader = injector.get(LazyModuleLoader);
+    compiler = injector.get(Compiler);
+
+    lazyModuleLoader.add(lazymodules);
   });
 
-  it('loadContainedCustomElements()', async () => {
-    lazyModuleLoader.add(lazymodules);
+  it('load NgModuleFactory', async () => {
     const module_a = await lazyModuleLoader.load('AModule');
     expect(module_a.instance).toEqual('a-module');
     const module_a_again = await lazyModuleLoader.load('AModule');
     expect(module_a_again.instance).toEqual('a-module');
   });
+
+  it('load moduleRef', async () => {
+    const compilerSpy = spyOn(compiler, 'compileModuleAsync').and.returnValue(
+      Promise.resolve(new FakeModuleFactory('b-module'))
+    );
+    const module_b = await lazyModuleLoader.load('BModule');
+    expect(compilerSpy).toHaveBeenCalledTimes(1);
+    expect(module_b.instance).toEqual('b-module');
+  });
+
+  it('不重复运行初始化相同名称的module', async () => {
+    lazyModuleLoader.load('AModule').then(() => {
+      expect(lazyModuleLoader['toLoad'].get('AModule')).not.toEqual(1);
+    });
+    lazyModuleLoader.add([
+      [
+        {
+          name: 'AModule',
+          loadChildren: 1 as any,
+          matcher
+        }
+      ]
+    ]);
+  });
+
+  it('不重复运行初始化相同名称的module', async () => {
+    lazyModuleLoader.add([
+      [
+        {
+          name: 'AModule',
+          loadChildren: () => Promise.resolve(1),
+          matcher
+        }
+      ]
+    ]);
+
+    lazyModuleLoader.load('AModule').then(() => {
+      expect(lazyModuleLoader['toLoad'].get('AModule')).toEqual(1);
+    });
+  });
+
+  it('不重复运行 promise', async () => {
+    const compilerSpy = spyOn(compiler, 'compileModuleAsync').and.returnValue(
+      Promise.resolve(new FakeModuleFactory('b-module'))
+    );
+    await Promise.all([lazyModuleLoader.load('BModule'), lazyModuleLoader.load('BModule')]);
+    expect(compilerSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('load error', async () => {
+    const compilerSpy = spyOn(compiler, 'compileModuleAsync').and.returnValue(Promise.reject('promise error'));
+    try {
+      await lazyModuleLoader.load('BModule');
+    } catch (error) {
+      console.log('error', error);
+      expect(error).toEqual('promise error');
+    }
+    expect(compilerSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('not found name', async () => {
+    try {
+      await lazyModuleLoader.load('myName');
+    } catch (error) {
+      expect(error.message).toEqual('module not found:myName');
+    }
+  });
 });
+
+class FakeCustomElementModule {}
 
 class FakeModuleRef extends NgModuleRef<any> {
   instance: any;
