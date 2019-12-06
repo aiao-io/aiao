c1d9afa014132fcf54b2c01177bf7f2fb8a68f95
Jimmy Liu <hero63418@gmail.com>
2019-12-06 14:28:00 +0800

    update

--- libs/elements/jest.config.js
+++ libs/elements/jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/elements'
+  coverageDirectory: '../../coverage/libs/elements',
+  passWithNoTests: true
 };
