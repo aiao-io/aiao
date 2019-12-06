79adf8d8ea74bb72fe4dd9b9e72d32e65aab2398
Jimmy Liu <hero63418@gmail.com>
2019-12-06 16:03:09 +0800

    ci(actions): fix symlink

--- tools/scripts/symlink.ts
+++ tools/scripts/symlink.ts
@@ -1,8 +1,10 @@
+import { existsSync, mkdirSync } from 'fs';
 import { ensureSymlinkSync, rmdirSync } from 'fs-extra';
+
 console.log('ensureSymlink aiao');
-try {
-  rmdirSync('node_modules/@aiao', { recursive: true });
-} catch {}
-try {
-  ensureSymlinkSync('dist/libs', 'node_modules/@aiao');
-} catch {}
+rmdirSync('node_modules/@aiao', { recursive: true });
+
+if (!existsSync('dist/libs')) {
+  mkdirSync('dist/libs');
+}
+ensureSymlinkSync('dist/libs', 'node_modules/@aiao');
