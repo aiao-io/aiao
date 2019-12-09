ebeb5b220f8968a14f16e5af0a77a91266fd5ba8
Jimmy Liu <hero63418@gmail.com>
2019-12-09 15:22:55 +0800

    update

--- tools/scripts/symlink.ts
+++ tools/scripts/symlink.ts
@@ -2,9 +2,16 @@ import { existsSync, mkdirSync } from 'fs';
 import { ensureSymlinkSync, rmdirSync } from 'fs-extra';
 
 console.log('ensureSymlink aiao');
-rmdirSync('node_modules/@aiao', { recursive: true });
 
-if (!existsSync('dist/libs')) {
-  mkdirSync('dist/libs');
+const aiaoNpmPath = 'node_modules/@aiao';
+const libDistPath = 'dist/libs';
+
+if (existsSync(aiaoNpmPath)) {
+  rmdirSync(aiaoNpmPath, { recursive: true });
 }
-ensureSymlinkSync('dist/libs', 'node_modules/@aiao');
+
+if (!existsSync(libDistPath)) {
+  mkdirSync(libDistPath);
+}
+
+ensureSymlinkSync(libDistPath, aiaoNpmPath);
