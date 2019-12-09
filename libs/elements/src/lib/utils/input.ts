f2f70d1429d2b0681dc5839b308dfab7808ef44f
Jimmy Liu <hero63418@gmail.com>
2019-12-09 15:41:45 +0800

    update

--- libs/elements/src/lib/utils/input.ts
+++ libs/elements/src/lib/utils/input.ts
@@ -10,7 +10,7 @@ export const renderHiddenInput = (
   if (always || hasShadowDom(container)) {
     let input = container.querySelector('input.aux-input') as HTMLInputElement | null;
     if (!input) {
-      input = container.ownerDocument!.createElement('input');
+      input = container.ownerDocument.createElement('input');
       input.type = 'hidden';
       input.classList.add('aux-input');
       container.appendChild(input);
