0429ddd78657a2ca1606d0671d1160295513d5ec
liupeng <lpengneo@163.com>
2019-12-11 14:47:03 +0800

    update lazy component test

--- integration/lazy-component/cypress-e2e/src/integration/examples/lazy-component_spec.ts
+++ integration/lazy-component/cypress-e2e/src/integration/examples/lazy-component_spec.ts
@@ -6,15 +6,15 @@ describe('Lazy Component Loader', () => {
   it('open dialog without lazyComponentLoader', () => {
     cy.visit('/');
     cy.get('button')
-      .eq(0)
+      .contains('Open Dialog ( without LazyComponentLoader )')
       .click();
-    cy.get('app-dialog h2').contains('I am a dialog component in the home module');
+    cy.get('app-dialog').should('have.length', 0);
   });
 
   it('open dialog with lazyComponentLoader', () => {
     cy.visit('/');
     cy.get('button')
-      .eq(1)
+      .contains('Open Dialog( with LazyComponentLoader )')
       .click();
     cy.get('app-dialog h2').contains('I am a dialog component in the home module');
   });
@@ -24,7 +24,7 @@ describe('Home Page Open Dialog', () => [
   it('open dialog', () => {
     cy.visit('/home');
     cy.get('button')
-      .eq(0)
+      .contains('show dialog')
       .click();
     cy.get('app-dialog h2').contains('I am a dialog component in the home module');
   })
