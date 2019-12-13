7d73a2b8b4d8b0e8b119c7bbc7f3f3d689823487
Jimmy Liu <hero63418@gmail.com>
2019-12-13 19:08:12 +0800

    update

--- libs/typeorm-plus/jest.config.js
+++ libs/typeorm-plus/jest.config.js
@@ -2,8 +2,5 @@ module.exports = {
   name: 'typeorm-plus',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/typeorm-plus',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
+  snapshotSerializers: []
 };
