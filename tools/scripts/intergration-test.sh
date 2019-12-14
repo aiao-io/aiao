2daf3d5001a142c7d1e6fd1f078a769ee49888b1
Jimmy Liu <hero63418@gmail.com>
2019-12-14 22:00:04 +0800

    update

--- tools/scripts/intergration-test.sh
+++ tools/scripts/intergration-test.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 echo "integration test"
 
+port=6200
+
+SKIP_TEST=(
+  integration/stencil-toolkit-nx/
+  integration/lazy-module/
+)
+
 for D in integration/*/; do
-  sh -c "cd ${D} && yarn install --frozen-lockfile --non-interactive"
+  port=$(expr $port + 1)
+  if [[ "${SKIP_TEST[@]}" =~ "${D%%/}" ]]; then
+    echo "skip ${D}"
+  else
+    sh -c "cd ${D} && yarn install --frozen-lockfile --non-interactive && yarn run e2e"
+  fi
 done
