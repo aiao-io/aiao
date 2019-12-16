1ae4536773d3d7669ce10c5250db3f49213c8e3b
liupeng <lpengneo@163.com>
2019-12-16 16:38:21 +0800

    update markdown

--- apps/docs/src/app/markdown/markdown.ts
+++ apps/docs/src/app/markdown/markdown.ts
@@ -4,6 +4,27 @@ export const BriefMdEn = require('raw-loader!dist/apps/docs/docs/README.en.md');
 export const ColorMdCn = require('raw-loader!dist/apps/docs/docs/libs/color/README.md');
 export const ColorMdEn = require('raw-loader!dist/apps/docs/docs/libs/color/README.en.md');
 
+export const ColorChangeLogMdCn = require('raw-loader!dist/apps/docs/docs/libs/color/CHANGELOG.md');
+export const ColorChangeLogMdEn = require('raw-loader!dist/apps/docs/docs/libs/color/CHANGELOG.en.md');
+
+export const ElementsMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements/readme.md');
+// export const ElementsMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements/readme.en.md');
+
+export const ElementsEditorMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/elements-editor/readme.md');
+// export const ElementsEditorMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/elements-editor/readme.en.md');
+
+export const ElementsViewMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/elements-view/readme.md');
+// export const ElementsViewMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/elements-view/readme.en.md');
+
+export const ImgMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/img/readme.md');
+// export const ImgMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements/src/lib/components/img/readme.en.md');
+
+export const ElementsAngularMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements-angular/README.md');
+// export const ElementsAngularMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements-angular/README.EN.md');
+
+export const ElementsCdkMdCn = require('raw-loader!dist/apps/docs/docs/libs/elements-cdk/README.md');
+// export const ElementsAngularMdEn = require('raw-loader!dist/apps/docs/docs/libs/elements-cdk/README.EN.md');
+
 export const ImageStorageMdCn = require('raw-loader!dist/apps/docs/docs/libs/image-storage/README.md');
 // export const ImageStorageMdEn = require('raw-loader!dist/apps/docs/docs/libs/image-storage/README.en.md');
 
@@ -13,11 +34,23 @@ export const UtilMdEn = require('raw-loader!dist/apps/docs/docs/libs/util/README
 export const LazyComponentMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-component/README.md');
 export const LazyComponentMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-component/README.en.md');
 
+export const LazyComponentChangeLogMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-component/CHANGELOG.md');
+export const LazyComponentChangeLogMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-component/CHANGELOG.en.md');
+
 export const LazyElementMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-element/README.md');
 export const LazyElementMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-element/README.en.md');
+
+export const LazyElementChangeLogMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-element/CHANGELOG.md');
+export const LazyElementChangeLogMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-element/CHANGELOG.en.md');
 
 export const LazyModuleMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-module/README.md');
 export const LazyModuleMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-module/README.en.md');
 
+export const LazyModuleChangeLogMdCn = require('raw-loader!dist/apps/docs/docs/integration/lazy-module/CHANGELOG.md');
+export const LazyModuleChangeLogMdEn = require('raw-loader!dist/apps/docs/docs/integration/lazy-module/CHANGELOG.en.md');
+
 export const StencilToolkitMdCn = require('raw-loader!dist/apps/docs/docs/integration/stencil-toolkit-nx/README.md');
 export const StencilToolkitMdEn = require('raw-loader!dist/apps/docs/docs/integration/stencil-toolkit-nx/README.en.md');
+
+export const StencilToolkitChangeLogMdCn = require('raw-loader!dist/apps/docs/docs/integration/stencil-toolkit-nx/CHANGELOG.md');
+export const StencilToolkitChangeLogMdEn = require('raw-loader!dist/apps/docs/docs/integration/stencil-toolkit-nx/CHANGELOG.en.md');
