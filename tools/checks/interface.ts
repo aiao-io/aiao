64bff671b90352ad0c1800df32f4aff61bc9136d
Jimmy Liu <hero63418@gmail.com>
2019-12-17 12:29:40 +0800

    ci: add check-lodash

--- tools/checks/interface.ts
+++
@@ -1,4 +0,0 @@
-import { YargsAffectedOptions } from '@nrwl/workspace/src/command-line/run-tasks/affected';
-
-// tslint:disable-next-line: no-empty-interface
-export interface CommandBaseConfig extends YargsAffectedOptions {}
--- tools/scripts/affected-lodash-optimize.ts
+++ tools/scripts/affected-lodash-optimize.ts
@@ -1,14 +1,17 @@
+import Ora from 'ora';
+import { exit } from 'process';
 import { Project } from 'ts-morph';
+import { argv } from 'yargs';
 
+import { YargsAffectedOptions } from '@nrwl/workspace/src/command-line/run-tasks/affected';
 import { parseFiles } from '@nrwl/workspace/src/command-line/shared';
 
-import { CommandBaseConfig } from './interface';
-
-const findLodash = (...path: string[]) => {
+const fixLodash = (...path: string[]) => {
+  const ora = Ora();
+  ora.succeed('自动修复 lodash 引用:' + path);
   const project = new Project();
   project.addSourceFilesAtPaths(path);
   const sourceFiles = project.getSourceFiles(path);
-  const findFiles = [];
   sourceFiles.forEach(file => {
     const imports = file.getImportDeclarations().filter(d => {
       const name = d
@@ -20,23 +23,33 @@ const findLodash = (...path: string[]) => {
 
     if (imports.length > 0) {
       const filePath = file.getFilePath();
-      findFiles.push(filePath);
+      imports.forEach(d => {
+        d.getNamedImports().forEach(e => {
+          const name = e.getName();
+          file.addImportDeclaration({
+            defaultImport: name,
+            moduleSpecifier: `lodash/${name}`
+          });
+        });
+      });
+      imports.forEach(imp => imp.remove());
+      file.saveSync();
+      ora.succeed(filePath);
     }
   });
-  return findFiles;
+  ora.stop();
 };
 
-export const checkLodash = async (conf: CommandBaseConfig) => {
+export const autoFixLodash = async (conf: YargsAffectedOptions) => {
   const config = parseFiles(conf);
   const { files } = config;
   const needFiles = files.filter(
     d => d.endsWith('.ts') && !d.includes('e2e') && !d.includes('tools') && /(spec|po).ts$/.test(d) === false
   );
   if (needFiles.length > 0) {
-    const findFiles = findLodash(...needFiles);
-    if (findFiles.length > 0) {
-      findFiles.forEach((f, i) => console.error(`${i} ${f}`));
-      process.exit(1);
-    }
+    fixLodash(...needFiles);
   }
+  exit();
 };
+
+autoFixLodash(argv);
