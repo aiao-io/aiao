74d46387034414ff82fd4b45dbe56c03d4eb68ce
liupeng <lpengneo@163.com>
2019-12-23 15:23:39 +0800

    update

--- apps/docs/src/app/app.module.ts
+++ apps/docs/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
