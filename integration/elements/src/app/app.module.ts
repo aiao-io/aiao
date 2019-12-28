cd3abd3b211c1c5918bec9a71e1ffce0c523579d
Jimmy Liu <hero63418@gmail.com>
2019-12-28 11:18:05 +0800

    update

--- integration/elements/src/app/app.module.ts
+++ integration/elements/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AiaoElementsModule } from '@aiao/elements-angular';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AiaoElementsModule.forRoot({})],
   providers: [],
   bootstrap: [AppComponent]
 })
--- libs/elements/src/lib/components/code-editor/code-editor.tsx
+++ libs/elements/src/lib/components/code-editor/code-editor.tsx
@@ -34,7 +34,6 @@ let loader: any;
 })
 export class CodeEditor implements ComponentInterface {
   @Element() el!: HTMLAiaoCodeEditorElement;
-  private editorRef: HTMLElement;
   private resourcesUrl: string = config.get('resourcesUrl');
 
   private editor: monaco.editor.IStandaloneCodeEditor;
@@ -96,7 +95,7 @@ export class CodeEditor implements ComponentInterface {
 
     options.value = this.value;
     options.language = this.language;
-    this.editor = monaco.editor.create(this.editorRef, options);
+    this.editor = monaco.editor.create(this.el, options);
 
     const updateValue = (action?: 'input') => {
       const value = this.editor.getValue();
@@ -131,13 +130,6 @@ export class CodeEditor implements ComponentInterface {
 
   render() {
     renderHiddenInput(true, this.el, this.name, this.value, this.disabled);
-    if (this.editorRef && monaco) {
-      this.createMonaco();
-    }
-    return (
-      <Host>
-        <div class="editor-container" ref={ref => (this.editorRef = ref)}></div>
-      </Host>
-    );
+    return <Host></Host>;
   }
 }
