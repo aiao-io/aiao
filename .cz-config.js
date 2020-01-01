8999cb3f5d9796501fdce9bffab1d27b42bb1332
Jimmy Liu <hero63418@gmail.com>
2020-01-01 13:20:57 +0800

    update

---
+++ .cz-config.js
@@ -0,0 +1,119 @@
+const base = {
+  scopes: [
+    { name: 'color' },
+    { name: 'elements' },
+    { name: 'elements-angular' },
+    { name: 'elements-cdk' },
+    { name: 'elements-react' },
+    { name: 'image-storage' },
+    { name: 'lazy-component' },
+    { name: 'lazy-element' },
+    { name: 'lazy-module' },
+    { name: 'stencil-toolkit' },
+    { name: 'typeorm-plus' }
+  ],
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
+  // limit subject length
+  subjectLimit: 100
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
+
+const en = {
+  ...base,
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'style',
+      name:
+        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature'
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance'
+    },
+    { value: 'test', name: 'test:     Adding missing tests' },
+    {
+      value: 'chore',
+      name:
+        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
+    },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'WIP', name: 'WIP:      Work in progress' }
+  ],
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+  }
+};
+
+const zh_CN = {
+  ...base,
+  types: [
+    { value: 'feat', name: 'feat:     ✨ 新功能' },
+    { value: 'fix', name: 'fix:      🐛 修复错误' },
+    { value: 'docs', name: 'docs:     📖 修改文档' },
+    { value: 'style', name: 'style:    💎 代码格式样式' },
+    {
+      value: 'refactor',
+      name: 'refactor: 📦 重构了代码非功能也非修复错误'
+    },
+    { value: 'perf', name: 'perf:     🚀 改进性能' },
+    { value: 'test', name: 'test:     🚨 增加测试' },
+    {
+      value: 'chore',
+      name: 'chore:    构建过程辅助工具的修改'
+    },
+    { value: 'WIP', name: 'WIP:      临时提交还在进行中' }
+  ],
+  // override the messages, defaults are as follows
+  messages: {
+    type: '选择您要提交的更改类型：',
+    scope: '\n更改的范围 (可选):',
+    // used if allowCustomScopes is true
+    customScope: '提交的范围:',
+    subject: '写一个简短的，命令式的代码提交描述：\n',
+    body: '请提供更长的更改提交描述，使用 "|" 符号换行:\n',
+    breaking: '描述不兼容更新：\n',
+    footer: '添加问题编号参考 (例如："fix #123", "re #123".)\n',
+    confirmCommit: '大哥最后再确认次?'
+  }
+};
+
+const config = process.env.LANG.includes('zh_CN') ? zh_CN : en;
+
+module.exports = config;
---
+++ tools/scripts/commit-lint.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+console.log('🐟🐟🐟 Validating git commit message 🐟🐟🐟');
+const gitMessage = require('child_process')
+  .execSync('git log -1 --no-merges')
+  .toString()
+  .trim();
+
+const matchCommit = /(chore|feat|fix|cleanup|docs)\((angular|bazel|core|docs|nextjs|node|react|storybook|testing|repo|misc)\):\s(([a-z0-9:\-\s])+)/g.test(
+  gitMessage
+);
+const matchRevert = /Revert/gi.test(gitMessage);
+const matchRelease = /Release/gi.test(gitMessage);
+const exitCode = +!(matchRelease || matchRevert || matchCommit);
+
+if (exitCode === 0) {
+  console.log('Commit ACCEPTED 👌');
+} else {
+  console.log(
+    '[Error]: Ho no! 😦 Your commit message: \n' +
+      '-------------------------------------------------------------------\n' +
+      gitMessage +
+      '\n-------------------------------------------------------------------' +
+      '\n\n 👉️ Does not follow the commit message convention specified in the CONTRIBUTING.MD file.'
+  );
+  console.log('\ntype(scope): subject \n BLANK LINE \n body');
+  console.log('\n');
+  console.log('possible types: chore|build|feat|fix|cleanup|docs');
+  console.log(
+    'possible scopes: angular|bazel|core|docs|nextjs|node|react|storybook|testing|repo|misc (if unsure use "core")'
+  );
+  console.log('\nEXAMPLE: \n' + 'feat(nx): add an option to generate lazy-loadable modules\n');
+}
+process.exit(exitCode);
