b8c0b61add0793e24ff977d32cfa89e919dd5f68
liupeng <lpengneo@163.com>
2020-01-07 18:56:10 +0800

    update

--- tools/scripts/generate-nav.ts
+++ tools/scripts/generate-nav.ts
@@ -4,32 +4,29 @@ const path = require('path');
 //设置md文件位置
 const mdFilePath = 'dist/apps/docs/docs';
 
+const destinationFile = 'apps/docs/src/assets/navigation.json';
+
 const arr = [];
 
 //调用函数遍历根目录，同时传递 文件夹路径和对应的数组
 //请使用同步读取
 generateNavigation(mdFilePath, arr);
 //读取完毕则写入到json文件中
-fs.writeFileSync('apps/docs/src/assets/navigation.json', JSON.stringify(arr));
+fs.writeFileSync(destinationFile, JSON.stringify(arr));
 
 function generateNavigation(dirPath: string, ary: any[], parentName = '', parentPath = '') {
-  let filesList = fs.readdirSync(dirPath);
+  const filesList = fs.readdirSync(dirPath);
   const routerPath = resolvePath(mdFilePath, parentPath);
-
-  // if (hasAllMd(filesList) && !parentName) {
-  //   addIntroductionItem(ary, routerPath, parentName, parentPath);
-  // }
 
   if (hasMdAndDir(filesList)) {
     addIntroductionItem(ary, routerPath, parentName, parentPath);
-    filesList = filterDir(filesList);
-    retrieveFilesList(filesList, ary, dirPath);
+    retrieveFilesList(filterDir(filesList), ary, dirPath, 1);
   } else {
     retrieveFilesList(filesList, ary, dirPath);
   }
 }
 
-function retrieveFilesList(filesList: any[], ary: any[], dirPath: string) {
+function retrieveFilesList(filesList: any[], ary: any[], dirPath: string, indexPlus = 0) {
   for (let i = 0; i < filesList.length; i++) {
     //描述此文件/文件夹的对象
     const fileObj: any = {};
@@ -45,7 +42,7 @@ function retrieveFilesList(filesList: any[], ary: any[], dirPath: string) {
       fileObj.children = [];
       ary.push(fileObj);
       //递归调用
-      generateNavigation(filePath, ary[i].children, filesList[i], filePath);
+      generateNavigation(filePath, ary[i + indexPlus].children, filesList[i], filePath);
     }
   }
 }
@@ -64,18 +61,19 @@ function addIntroductionItem(ary: any[], routerPath: string, parentName: string,
 }
 
 function filterDir(ary: []) {
-  return ary.filter(fl => !/(README|CHANGELOG).*?d/gi.test(fl));
+  return ary.filter(fl => !/.md$/i.test(fl));
 }
 
 function hasAllMd(ary: []) {
-  return ary.every(fl => /(README|CHANGELOG).*?d/gi.test(fl));
+  return ary.every(fl => /.md$/i.test(fl));
 }
+
 function hasMdAndDir(ary: []) {
-  return ary.some(fl => /(README|CHANGELOG).*?d/gi.test(fl)) && ary.some(fl => !/(README|CHANGELOG).*?d/gi.test(fl));
+  return ary.some(fl => /.md$/i.test(fl)) && ary.some(fl => !/.md$/i.test(fl));
 }
 
 function resolvePath(regex: string, innerPath: string) {
   const reg = new RegExp(regex);
   const _path = innerPath.replace(reg, '');
-  return _path.replace(/\/(README|CHANGELOG).*?d/gi, '');
+  return _path.replace(/\/.*?md/gi, '');
 }
