b97f06d3f3a1ecf9b1dedbcd99dca0da6aca902c
Jimmy Liu <hero63418@gmail.com>
2020-02-12 12:41:23 +0800

    update

--- libs/date/src/index.ts
+++ libs/date/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/date.module';
+export { add } from './lib/add';
+export { addBusinessDays } from './lib/addBusinessDays';
+export { addDays } from './lib/addDays';
+export { addHours } from './lib/addHours';
+export { addMilliseconds } from './lib/addMilliseconds';
+export { addMinuts } from './lib/addMinuts';
+export { addMonths } from './lib/addMonths';
+export { addQuarters } from './lib/addQuarters';
+export { addSeconds } from './lib/addSeconds';
+export { addWeeks } from './lib/addWeeks';
+export { addYears } from './lib/addYears';
+export { getDaysInMonth } from './lib/getDaysInMonth';
+export { getUnixTimestamp } from './lib/getUnixTimestamp';
+export { isISODateString } from './lib/isISODateString';
+export { isWeekend } from './lib/isWeekend';
