f144cdb69363283d489de233c7fad0e4581b2063
Jimmy Liu <hero63418@gmail.com>
2020-02-17 17:48:28 +0800

    update

---
+++ libs/elements-react/src/lib/proxies.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@aiao/elements';
+
+import { defineCustomElements, applyPolyfills } from '@aiao/elements/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const AiaoCodeDiffEditor = /*@__PURE__*/createReactComponent<JSX.AiaoCodeDiffEditor, HTMLAiaoCodeDiffEditorElement>('aiao-code-diff-editor');
+export const AiaoCodeEditor = /*@__PURE__*/createReactComponent<JSX.AiaoCodeEditor, HTMLAiaoCodeEditorElement>('aiao-code-editor');
+export const AiaoElementsEditor = /*@__PURE__*/createReactComponent<JSX.AiaoElementsEditor, HTMLAiaoElementsEditorElement>('aiao-elements-editor');
+export const AiaoElementsEditorPreview = /*@__PURE__*/createReactComponent<JSX.AiaoElementsEditorPreview, HTMLAiaoElementsEditorPreviewElement>('aiao-elements-editor-preview');
+export const AiaoElementsForm = /*@__PURE__*/createReactComponent<JSX.AiaoElementsForm, HTMLAiaoElementsFormElement>('aiao-elements-form');
+export const AiaoElementsView = /*@__PURE__*/createReactComponent<JSX.AiaoElementsView, HTMLAiaoElementsViewElement>('aiao-elements-view');
+export const AiaoImg = /*@__PURE__*/createReactComponent<JSX.AiaoImg, HTMLAiaoImgElement>('aiao-img');
