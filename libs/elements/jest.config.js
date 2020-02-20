d2b43a5ec74a6173d69c4ce3e0b1e5c7d8bb0d4a
Jimmy Liu <hero63418@gmail.com>
2020-02-20 14:23:59 +0800

    update

--- libs/elements/jest.config.js
+++ libs/elements/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
+  coverageDirectory: '../../coverage/libs/elements',
+  coverageReporters: ['html', 'json'],
   collectCoverageFrom: ['./src/lib/**/*.ts', './src/lib/**/*.tsx'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/elements'
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html']
 };
