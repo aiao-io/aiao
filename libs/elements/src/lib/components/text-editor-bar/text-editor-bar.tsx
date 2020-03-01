0f9532784667d92bcaa486862e0f29e1b47ba814
Jimmy Liu <hero63418@gmail.com>
2020-03-01 13:35:11 +0800

    update

--- libs/elements/src/lib/components/text-editor-bar/text-editor-bar.tsx
+++ libs/elements/src/lib/components/text-editor-bar/text-editor-bar.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Host, Prop, EventEmitter, Event } from '@stencil/core';
 
 import { config } from '../../global/config';
 import { TextEditorAcitons } from '../../interfaces/text-editor.interface';
@@ -10,7 +10,7 @@ import { TextEditorAcitons } from '../../interfaces/text-editor.interface';
   shadow: true
 })
 export class TextEditorBar {
-  private options = ['1', '2', '3', '4', '5', '6', '7'];
+  // private options = ['1', '2', '3', '4', '5', '6', '7'];
   private resourcesUrl: string = config.get('resourcesUrl');
 
   private defaultSettings = [
@@ -71,10 +71,6 @@ export class TextEditorBar {
     },
     {
       action: TextEditorAcitons.Italic,
-      title: 'line'
-    },
-    {
-      action: TextEditorAcitons.Italic,
       icon: 'indent.svg',
       title: 'indent'
     },
@@ -105,103 +101,32 @@ export class TextEditorBar {
     },
     {
       action: TextEditorAcitons.Italic,
-      icon: 'unlink.svg',
-      title: 'unlink'
+      icon: 'undo.svg',
+      title: 'undo'
+    },
+    {
+      action: TextEditorAcitons.Italic,
+      icon: 'redo.svg',
+      title: 'redo'
     }
   ];
+
+  @Event() action: EventEmitter<any>;
 
   @Prop() actions = this.defaultSettings;
 
   render() {
     return (
-      <div class="action-bar">
-        {this.actions.map(({ icon, title }) => {
+      <Host class="action-bar">
+        {this.actions.map(({ action, icon, title }) => {
           const src = `${this.resourcesUrl}/text-editor/${icon}`;
-          return <button class="action-button">{icon ? <ion-icon title={title} src={src} /> : title}</button>;
+          return (
+            <button class="action-button" onClick={() => this.action.emit({ detail: action })}>
+              {icon ? <ion-icon title={title} src={src} /> : title}
+            </button>
+          );
         })}
-      </div>
+      </Host>
     );
   }
 }
-
-/*
-<div class="action-bar">
-        <ion-button fill={bold ? 'solid' : 'clear'} onClick={() => this.bold()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/bold.svg`} />
-        </ion-button>
-        <ion-button fill={italic ? 'solid' : 'clear'} onClick={() => this.italic()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/italic.svg`} />
-        </ion-button>
-        <ion-button fill={underline ? 'solid' : 'clear'} onClick={() => this.underline()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/underline.svg`} />
-        </ion-button>
-        <ion-button fill={strikeThrough ? 'solid' : 'clear'} onClick={() => this.strikethrough()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/strikeThrough.svg`} />
-        </ion-button>
-        <ion-button fill={h1 ? 'solid' : 'clear'} onClick={() => this.heading(1)}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/h1.svg`} />
-        </ion-button>
-        <ion-button fill={h2 ? 'solid' : 'clear'} onClick={() => this.heading(2)}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/h2.svg`} />
-        </ion-button>
-        <ion-button fill={h3 ? 'solid' : 'clear'} onClick={() => this.heading(3)}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/h3.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.paragraph()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/paragraph.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.olist()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/list-ol.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.ulist()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/list-ul.svg`} />
-        </ion-button>
-        <ion-button fill={blockquote ? 'solid' : 'clear'} onClick={() => this.quote()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/quote.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.line()}>
-          <ion-icon name="return-down-back-outline"></ion-icon>
-        </ion-button>
-        <select onChange={e => this.fontSizeChange(e)}>
-          {this.options.map(option => (
-            <option value={option} selected={fontSize === option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        <mlab-color
-          value={foreColor}
-          onMlabOpen={() => this._saveSelection()}
-          onMlabChange={(ev: any) => this.foreColorChanged(ev)}
-        >
-          文字
-        </mlab-color>
-        <ion-button fill="clear" onClick={() => this.indent()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/indent.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.outdent()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/outdent.svg`} />
-        </ion-button>
-        <ion-button fill={alginLeft ? 'solid' : 'clear'} onClick={() => this.alginLeft()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/align-left.svg`} />
-        </ion-button>
-        <ion-button fill={alginCenter ? 'solid' : 'clear'} onClick={() => this.alginCenter()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/align-center.svg`} />
-        </ion-button>
-        <ion-button fill={alginRight ? 'solid' : 'clear'} onClick={() => this.alginRight()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/align-right.svg`} />
-        </ion-button>
-        <ion-button fill={alginFull ? 'solid' : 'clear'} onClick={() => this.alginFull()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/align-justify.svg`} />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.undo()}>
-          <ion-icon slot="icon-only" name="arrow-undo-outline"></ion-icon>
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.redo()}>
-          <ion-icon slot="icon-only" name="arrow-redo-outline" />
-        </ion-button>
-        <ion-button fill="clear" onClick={() => this.unlink()}>
-          <ion-icon slot="icon-only" src={`${this.resourcesUrl}/assets/text-editor/unlink.svg`} />
-        </ion-button>
-      </div>
-*/
