96a107062c3437c8c184ff72a040c5c397a545aa
Jimmy Liu <hero63418@gmail.com>
2020-03-03 12:38:16 +0800

    test(elements): add test

--- libs/elements/src/lib/utils/code-editor/base-url.ts
+++ libs/elements/src/lib/utils/code-editor/base-url.ts
@@ -1,5 +1,4 @@
 import { config } from '../../global/config';
 
-export const getBaseUrl = (baseUrl: string) => {
-  return baseUrl || config.get('codeEditorBaseUrl') || 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.2/min';
-};
+export const getBaseUrl = (baseUrl: string) =>
+  baseUrl || config.get('codeEditorBaseUrl') || 'https://cdn.jsdelivr.net/npm/monaco-editor@0.20.0/min';
