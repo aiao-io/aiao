65b4a1079643b7c7cc29b0f692445c06496d5946
Jimmy Liu <hero63418@gmail.com>
2020-03-22 13:53:04 +0800

    update

--- apps/dev-elements-react/src/app/elements-editor/ElementsEditor.spec.tsx
+++ apps/dev-elements-react/src/app/elements-editor/ElementsEditor.spec.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import ELementsEditorPage from './ELementsEditor';
+import ElementsEditorPage from './ElementsEditor';
 
 describe('ELementsEditorPage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ELementsEditorPage />);
+    const { baseElement } = render(<ElementsEditorPage />);
     expect(baseElement).toBeTruthy();
   });
 });
