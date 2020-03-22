0a1e586b5ab46629b7e83c96f5648c334464ebad
Jimmy Liu <hero63418@gmail.com>
2020-03-22 18:53:26 +0800

    update

--- libs/elements/stencil.dev.config.ts
+++ libs/elements/stencil.dev.config.ts
@@ -1,5 +1,3 @@
-import { join, resolve } from 'path';
-
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
@@ -15,34 +13,7 @@ export const config: Config = {
       type: 'dist-hydrate-script'
     }
   ],
-  copy: [
-    // {
-    //   src: join(__dirname, '../../', 'dist/libs/image-storage'),
-    //   dest: join(__dirname, 'vendors/image-storage')
-    // },
-    {
-      src: resolve(__dirname, '../../', 'node_modules/monaco-editor/dev'),
-      dest: resolve(__dirname, 'vendors/monaco-editor'),
-      keepDirStructure: true
-    },
-    {
-      src: resolve(__dirname, '../../', 'node_modules/@ionic/core'),
-      dest: resolve(__dirname, 'vendors/ionic'),
-      keepDirStructure: true
-    }
-  ],
   tsconfig: 'tsconfig.json',
   enableCache: true,
   globalScript: 'src/lib/global/global.ts'
 };
-
-console.log(
-  {
-    src: join(__dirname, '../../', 'node_modules/monaco-editor/dev'),
-    dest: join(__dirname, 'vendors/monaco-editor')
-  },
-  {
-    src: join(__dirname, '../../', 'node_modules/@ionic/core'),
-    dest: join(__dirname, 'vendors/ionic')
-  }
-);
