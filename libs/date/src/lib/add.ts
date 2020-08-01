0a58b7b2d2643aef745cdbf2e46df6d5a941284e
刘磊 <liulei9@ruijie.com.cn>
2020-08-01 13:37:51 +0800

    优化 date

--- libs/date/src/lib/add.ts
+++ libs/date/src/lib/add.ts
@@ -15,13 +15,13 @@ interface IAddDate {
 // 添加时间
 export const add = (date: Date, values: IAddDate) => {
   let { years, months, weeks, days, hours, minutes, seconds } = values;
-  years = 'years' in values ? years : 0;
-  months = 'months' in values ? months : 0;
-  weeks = 'weeks' in values ? weeks : 0;
-  days = 'days' in values ? days : 0;
-  hours = 'hours' in values ? hours : 0;
-  minutes = 'minutes' in values ? minutes : 0;
-  seconds = 'seconds' in values ? seconds : 0;
+  years = years || 0;
+  months = months || 0;
+  weeks = weeks || 0;
+  days = days || 0;
+  hours = hours || 0;
+  minutes = minutes || 0;
+  seconds = seconds || 0;
   let newDate = addMonths(date, months + years * 12);
   newDate = addDays(newDate, days + weeks * 7);
   const minutesToAdd = minutes + hours * 60;
