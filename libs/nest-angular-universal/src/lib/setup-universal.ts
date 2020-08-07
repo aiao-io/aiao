fe2595b6280312808275c74677c643d44817a29b
Jimmy <hero63418@gmail.com>
2020-08-07 18:26:30 +0800

    123

--- libs/nest-angular-universal/src/lib/setup-universal.ts
+++ libs/nest-angular-universal/src/lib/setup-universal.ts
@@ -1,14 +1,65 @@
 import { FastifyInstance } from 'fastify';
 import fastifyStatic from 'fastify-static';
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+import { ngFastilyEngine } from '@aiao/universal-fastify-engine';
 
 import { NestUniversalOptions } from './interface';
 
+const promiseExistFile = (indexHtml: string) => {
+  return new Promise(resolve => {
+    const pt = () => {
+      if (existsSync(indexHtml)) {
+        resolve(true);
+      } else {
+        console.log(`waiting browser build`);
+        setTimeout(() => {
+          pt();
+        }, 500);
+      }
+    };
+    pt();
+  });
+};
+
 export const setupUniversal = async (app: FastifyInstance, options: NestUniversalOptions) => {
-  const { distPath, production } = options;
+  const {
+    bootstrap,
+    defaultLocale,
+    distPath,
+    document,
+    documentFilePath,
+    locales,
+    providers,
+    production,
+    browserHost,
+    browserPort
+  } = options;
+
+  app.register(ngFastilyEngine, {
+    bootstrap,
+    defaultLocale,
+    distPath,
+    document,
+    documentFilePath,
+    locales,
+    providers
+  });
 
   if (!production) {
-    const proxy = await import('./proxy-browser-sync-client');
-    await proxy.proxyBrowserSyncClient(app, options);
+    if (locales && locales?.length > 0) {
+      const proxy = await import('fastify-http-proxy');
+      locales.forEach(locale => {
+        app.register(proxy.default, {
+          upstream: `http://${browserHost || 'localhost'}:${browserPort || 4200}/browser-sync/browser-sync-client.js`,
+          prefix: `/${locale}/browser-sync/browser-sync-client.js`
+        });
+      });
+      await promiseExistFile(join(distPath, defaultLocale || locales[0], 'index.html'));
+    } else {
+      await promiseExistFile(join(distPath, 'index.html'));
+    }
   }
 
   app.register((instance: FastifyInstance, opts: any, next: () => void) => {
