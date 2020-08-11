44f76eaa803cfccffb31c3e43a93c822be7d5dbc
Jimmy Liu <hero63418@gmail.com>
2020-08-12 00:53:48 +0800

    23213

--- libs/elements-react/src/index.ts
+++ libs/elements-react/src/index.ts
@@ -1,2 +1,8 @@
+import { applyPolyfills, defineCustomElements } from '@aiao/elements/loader';
+
+if (typeof window !== 'undefined') {
+  applyPolyfills().then(() => defineCustomElements(window));
+}
+
 export * from './lib/react-component-lib';
 export * from './lib/proxies';
