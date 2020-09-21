5137ec95882be75663fab0081330ca4c01a69f40
Jimmy <hero63418@gmail.com>
2020-09-21 19:32:30 +0800

    test(util): 优化测试

--- libs/util/src/lib/function.spec.ts
+++ libs/util/src/lib/function.spec.ts
@@ -1,49 +1,51 @@
 import { debounce, throttle } from './function';
 
 describe('function', () => {
-  it('should be call one', done => {
-    const start = new Date().getTime();
-    let runTime = 0;
-    let end: number;
-    function isDone() {
-      if (runTime === 1 && end >= 19) {
+  describe('debounce', () => {
+    it('should be call one', done => {
+      const start = new Date().getTime();
+      let runTime = 0;
+      let end: number;
+      const isDone = () => {
+        expect(runTime).toEqual(1);
+        expect(end).toBeGreaterThanOrEqual(19);
         done();
-      }
-    }
+      };
 
-    const debounceFun = debounce(() => {
-      end = new Date().getTime() - start;
-      runTime++;
-    }, 10);
-    debounceFun();
-    debounceFun();
-    setTimeout(() => {
+      const debounceFun = debounce(() => {
+        end = new Date().getTime() - start;
+        runTime++;
+      }, 10);
       debounceFun();
-    }, 9);
-    setTimeout(isDone, 50);
-  });
-
-  it('should be call one', done => {
-    const start = new Date().getTime();
-    let runTime = 0;
-    let end: number;
-    function isDone() {
-      if (runTime === 2) {
-        done();
-      }
-    }
-
-    const throttleFun = throttle(() => {
-      end = new Date().getTime() - start;
-      runTime++;
-    }, 10);
-    throttleFun();
-    setTimeout(() => {
-      throttleFun();
+      debounceFun();
+      setTimeout(() => {
+        debounceFun();
+      }, 9);
+      setTimeout(isDone, 50);
     });
-    setTimeout(() => {
+  });
+  describe('throttle', () => {
+    it('ok', done => {
+      const start = new Date().getTime();
+      let runTime = 0;
+      let end: number;
+      const isDone = () => {
+        expect(runTime).toEqual(2);
+        done();
+      };
+
+      const throttleFun = throttle(() => {
+        end = new Date().getTime() - start;
+        runTime++;
+      }, 10);
       throttleFun();
-    }, 10);
-    setTimeout(isDone, 50);
+      setTimeout(() => {
+        throttleFun();
+      });
+      setTimeout(() => {
+        throttleFun();
+      }, 10);
+      setTimeout(isDone, 50);
+    });
   });
 });
