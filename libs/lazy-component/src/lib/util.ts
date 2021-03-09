75353ac00f6fe33c77d0bbf2cc83563da6d3a1e3
Jimmy <hero63418@gmail.com>
2021-03-09 15:57:26 +0800

    12312

--- libs/lazy-component/src/lib/util.ts
+++ libs/lazy-component/src/lib/util.ts
@@ -1,11 +1,12 @@
 import { ComponentFactory, NgModuleRef, Type } from '@angular/core';
 
 export const findComponentFromModuleRef = (moduleRef: NgModuleRef<any>, selector: string) => {
-  // @angular < 9 或是 > 9 ivy 不开启
+  // @angular < 9 或是 >= 9 ivy 不开启
   const factories: Map<any, ComponentFactory<any>> = (moduleRef.componentFactoryResolver as any)._factories;
   if (factories) {
     return Array.from(factories.keys()).find(type => factories.get(type)!.selector === selector);
   }
+  // 需要在模块中提供组件索引，customElementComponents
   const customElementComponents: Type<any>[] = (moduleRef.instance as any)?.customElementComponents as any;
   if (customElementComponents) {
     return customElementComponents.find((d: any) => {
