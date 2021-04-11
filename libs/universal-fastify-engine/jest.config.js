90c012e73b8218801a45e4a443eea13312d58bbe
Jimmy.Liu <hero63418@gmail.com>
2021-04-11 22:54:00 +0800

    update

--- libs/universal-fastify-engine/jest.config.js
+++ libs/universal-fastify-engine/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  name: 'universal-fastify-engine',
+  displayName: 'universal-fastify-engine',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json'
     }
   },
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/universal-fastify-engine'
 };
