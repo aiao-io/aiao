cea175b9816e18365ab80a2616f50ebe009733ef
Jimmy Liu <hero63418@gmail.com>
2021-04-19 12:03:41 +0800

    update

--- apps/dev-nest-angular-universal/jest.config.js
+++ apps/dev-nest-angular-universal/jest.config.js
@@ -1,21 +1,6 @@
 module.exports = {
-  name: 'dev-nest-angular-universal',
+  displayName: 'dev-nest-angular-universal',
   preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/apps/dev-nest-angular-universal',
-  collectCoverageFrom: [
-    './src/**/*.ts',
-    '!./src/environments/**',
-    '!./src/main.ts',
-    '!./src/main.server.ts',
-    '!./src/app/app.server.module.ts',
-    '!./src/zone-flsgs.ts',
-    '!./src/polyfills.ts'
-  ],
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ],
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -25,5 +10,11 @@ module.exports = {
         before: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer']
       }
     }
-  }
+  },
+  coverageDirectory: '../../coverage/apps/dev-nest-angular-universal',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
 };
