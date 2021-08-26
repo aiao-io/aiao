9f658035eb9d8492e88c88157eeb307c65c4f3bb
Jimmy <hero63418@gmail.com>
2021-08-26 13:25:02 +0800

    123213

--- apps/dev-elements-angular-e2e/src/plugins/index.js
+++ apps/dev-elements-angular-e2e/src/plugins/index.js
@@ -11,8 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
