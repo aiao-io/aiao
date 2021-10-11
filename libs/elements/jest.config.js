f115a6c1cc244ae2e6f63c7fcbd59a24857016e3
Jimmy Liu <hero63418@gmail.com>
2021-10-11 23:30:59 +0800

    123

--- libs/elements/jest.config.js
+++ libs/elements/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'elements',
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/elements',
-  collectCoverageFrom: ['./src/lib/**/*.ts', '!src/lib/global/global.ts'],
+  collectCoverageFrom: ['./src/lib/**/*.ts', '!./src/lib/**/*.interface.ts', '!src/lib/global/global.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } }
 };
