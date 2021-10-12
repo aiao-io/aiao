22b99d9de26454c60154524f9ed40a361d3f8932
Jimmy <hero63418@gmail.com>
2021-10-12 08:00:46 +0800

    test(aiao): add test (#133)

---
+++ libs/elements/src/lib/components/tree/util.spec.ts
@@ -0,0 +1,81 @@
+import { treeNodeDateToTreeState } from './util';
+
+describe('true-util', () => {
+  it('treeNodeDateToTreeState', () => {
+    const data = [
+      { id: 'a', name: 'a' },
+      { id: 'b', name: 'b', parentId: 'a' }
+    ];
+    const newData = treeNodeDateToTreeState(data);
+    expect(newData).toEqual([{ children: [{ id: 'b', name: 'b', parentId: 'a' }], id: 'a', name: 'a' }]);
+  });
+
+  it('treeNodeDateToTreeState sort', () => {
+    const data = [
+      { id: 'a', name: 'a' },
+      { id: 'b', name: 'b', parentId: 'a', sort: 1 },
+      { id: 'b2', name: 'b2', parentId: 'a', sort: 2 }
+    ];
+    const newData = treeNodeDateToTreeState(data);
+    expect(newData).toEqual([
+      {
+        id: 'a',
+        name: 'a',
+        children: [
+          { id: 'b', name: 'b', parentId: 'a', sort: 1 },
+          { id: 'b2', name: 'b2', parentId: 'a', sort: 2 }
+        ]
+      }
+    ]);
+  });
+
+  it('treeNodeDateToTreeState sort', () => {
+    const data = [
+      { id: 'a', name: 'a' },
+      { id: 'b', name: 'b', parentId: 'a' },
+      { id: 'b2', name: 'b2', parentId: 'a', sort: 2 }
+    ];
+    const newData = treeNodeDateToTreeState(data);
+    expect(newData).toEqual([
+      {
+        id: 'a',
+        name: 'a',
+        children: [
+          { id: 'b', name: 'b', parentId: 'a' },
+          { id: 'b2', name: 'b2', parentId: 'a', sort: 2 }
+        ]
+      }
+    ]);
+  });
+
+  it('treeNodeDateToTreeState sort', () => {
+    const data = [
+      { id: 'a', name: 'a' },
+      { id: 'b', name: 'b', parentId: 'a', sort: 1 },
+      { id: 'b2', name: 'b2', parentId: 'a', sort: -2 }
+    ];
+    const newData = treeNodeDateToTreeState(data);
+    expect(newData).toEqual([
+      {
+        id: 'a',
+        name: 'a',
+        children: [
+          { id: 'b2', name: 'b2', parentId: 'a', sort: -2 },
+          { id: 'b', name: 'b', parentId: 'a', sort: 1 }
+        ]
+      }
+    ]);
+  });
+
+  it('treeNodeDateToTreeState sort', () => {
+    const data = [
+      { id: 'b', name: 'b', sort: 1 },
+      { id: 'b2', name: 'b2', sort: -2 }
+    ];
+    const newData = treeNodeDateToTreeState(data);
+    expect(newData).toEqual([
+      { id: 'b2', name: 'b2', sort: -2 },
+      { id: 'b', name: 'b', sort: 1 }
+    ]);
+  });
+});
--- libs/util/src/lib/lodash.spec.ts
+++ libs/util/src/lib/lodash.spec.ts
@@ -1,4 +1,4 @@
-import { getTag } from './lodash';
+import { getTag, toInteger } from './lodash';
 
 describe('getTag', () => {
   it('object', () => {
@@ -26,5 +26,37 @@ describe('getTag', () => {
   });
   it('undefined', () => {
     expect(getTag(undefined)).toEqual('[object Undefined]');
+  });
+
+  describe('toInteger', () => {
+    it('NaN', () => {
+      expect(toInteger(undefined)).toEqual(NaN);
+      expect(toInteger(null)).toEqual(NaN);
+      expect(toInteger(true)).toEqual(NaN);
+      expect(toInteger(false)).toEqual(NaN);
+      expect(toInteger(void 0)).toEqual(NaN);
+      expect(toInteger(NaN)).toEqual(NaN);
+    });
+    it('string to number', () => {
+      expect(toInteger('1')).toEqual(1);
+      expect(toInteger('01')).toEqual(1);
+      expect(toInteger('001')).toEqual(1);
+      expect(toInteger('0001')).toEqual(1);
+      expect(toInteger('00001')).toEqual(1);
+    });
+    it('-string to number', () => {
+      expect(toInteger('-1')).toEqual(-1);
+      expect(toInteger('-01')).toEqual(-1);
+      expect(toInteger('-001')).toEqual(-1);
+      expect(toInteger('-0001')).toEqual(-1);
+      expect(toInteger('-00001')).toEqual(-1);
+    });
+    it('string to number', () => {
+      expect(toInteger('1')).toEqual(1);
+      expect(toInteger('0.1')).toEqual(0);
+      expect(toInteger('0.01')).toEqual(0);
+      expect(toInteger('0.001')).toEqual(0);
+      expect(toInteger('0.0001')).toEqual(0);
+    });
   });
 });
