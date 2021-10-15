5b42b963e5b7621c9393cfe1d8c8261a1730bf52
Jimmy <hero63418@gmail.com>
2021-10-15 13:27:01 +0800

    12

--- libs/typeorm-plus/src/lib/interface.ts
+++ libs/typeorm-plus/src/lib/interface.ts
@@ -1,3 +1,26 @@
-import { Model, ModelCtor } from 'sequelize';
+import {
+  BulkCreateOptions,
+  CreateOptions,
+  FindOptions,
+  Identifier,
+  IncrementDecrementOptions,
+  IncrementDecrementOptionsWithBy,
+  ModelType,
+  NonNullFindOptions
+} from 'sequelize';
 
-export type SequelizeRepository<M> = ModelCtor<Model<M>>;
+export interface SequelizeRepository<M> extends ModelType<M> {
+  findAll(options?: FindOptions): Promise<M[]>;
+  findByPk(identifier?: Identifier, options?: Omit<FindOptions | NonNullFindOptions, 'where'>): Promise<M | null>;
+
+  findOne(options?: FindOptions): Promise<M | null>;
+  findOne(options: NonNullFindOptions): Promise<M>;
+
+  create(values?: any, options?: CreateOptions): Promise<M>;
+  create(values: any, options: CreateOptions & { returning: false }): Promise<void>;
+
+  bulkCreate(records: any[], options?: BulkCreateOptions): Promise<M[]>;
+
+  increment(field: string | string[], options: IncrementDecrementOptionsWithBy): Promise<M>;
+  increment(fields: { [key: string]: number }, options: IncrementDecrementOptions): Promise<M>;
+}
