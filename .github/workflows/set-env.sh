adb2ab467ef5df333ef5fd5d7c4780c2ee7dd9be
Jimmy Liu <hero63418@gmail.com>
2021-10-16 19:44:32 +0800

    2134

--- .github/workflows/set-env.sh
+++ .github/workflows/set-env.sh
@@ -2,7 +2,7 @@
 set -o errexit
 if [[ -n $GITHUB_BASE_REF ]]; then
   echo "Fetching Base Commit from GitHub"
-  echo "NX_BASE=$GITHUB_BASE_REF" >>$GITHUB_ENV
+  echo "NX_BASE=remotes/origin/$GITHUB_BASE_REF" >>$GITHUB_ENV
 else
   if [[ ! -f dist/ci/last-deploy.txt ]]; then
     mkdir -p dist/ci && git rev-list --max-parents=0 HEAD >dist/ci/last-deploy.txt
