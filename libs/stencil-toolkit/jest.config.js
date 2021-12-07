14ad99cab775eceabac64225e7fdcd6653c6ca1a
Jimmy <hero63418@gmail.com>
2021-12-07 14:30:41 +0800

    refactor(stencil-toolkit): 调整基础代码结构

--- libs/stencil-toolkit/jest.config.js
+++ libs/stencil-toolkit/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  name: 'stencil-toolkit',
+  displayName: 'stencil-toolkit',
   preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/stencil-toolkit',
-  collectCoverageFrom: ['./src/lib/**/*.ts'],
-
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment'
-  ],
-  testEnvironment: 'node'
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/stencil-toolkit'
 };
