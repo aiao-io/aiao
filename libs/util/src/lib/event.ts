53a55a14fd5d6cd7ce8c6272e9620e88574f2cd0
Jimmy <hero63418@gmail.com>
2022-02-15 14:10:25 +0800

    123123

--- libs/util/src/lib/event.ts
+++ libs/util/src/lib/event.ts
@@ -9,17 +9,6 @@ export type EvnetListener<T> = (event?: T[keyof T]) => void;
 export abstract class EventDispatcher<Events extends CustomEvents> {
   private m: Map<keyof Events, Set<EvnetListener<Events>>> = new Map();
 
-  private getListener(type: keyof Events): Set<any> {
-    const has = this.m.has(type);
-    if (has) {
-      return this.m.get(type) as any;
-    } else {
-      const listeners = new Set<any>();
-      this.m.set(type, listeners);
-      return listeners;
-    }
-  }
-
   addEventListener(type: keyof Events, listener: EvnetListener<Events>): void {
     this.getListener(type).add(listener);
   }
@@ -38,5 +27,16 @@ export abstract class EventDispatcher<Events extends CustomEvents> {
 
   removeAllEventListener(): void {
     this.m.clear();
+  }
+
+  private getListener(type: keyof Events): Set<any> {
+    const has = this.m.has(type);
+    if (has) {
+      return this.m.get(type) as any;
+    } else {
+      const listeners = new Set<any>();
+      this.m.set(type, listeners);
+      return listeners;
+    }
   }
 }
