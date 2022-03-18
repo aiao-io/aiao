3d5f1e06a69aab2a93b55146ff1c0c6f68308bb4
Jimmy <hero63418@gmail.com>
2022-03-18 22:34:27 +0800

    update

--- libs/nest-typeorm-plus/test/test-helper.ts
+++ libs/nest-typeorm-plus/test/test-helper.ts
@@ -1,22 +1,10 @@
-import { config } from 'dotenv';
-import { env } from 'process';
 import { DataSourceOptions } from 'typeorm';
 
-try {
-  config();
-} catch {}
-
-const {
-  TYPEORM_PLUS_TEST_DB_TYPE,
-  TYPEORM_PLUS_TEST_USERNAME,
-  TYPEORM_PLUS_TEST_PASSWORD,
-  TYPEORM_PLUS_TEST_DATABASE
-} = env;
-
 export const baseOptions: DataSourceOptions = {
-  type: (TYPEORM_PLUS_TEST_DB_TYPE as any) || 'postgres',
-  username: TYPEORM_PLUS_TEST_USERNAME || 'test',
-  password: TYPEORM_PLUS_TEST_PASSWORD || 'test',
-  database: TYPEORM_PLUS_TEST_DATABASE || 'test',
+  type: 'postgres',
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  port: 3210,
   synchronize: true
 };
