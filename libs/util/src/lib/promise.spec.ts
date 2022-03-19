3898b952bc0d8496a64b73c58756946e626f3747
Jimmy <hero63418@gmail.com>
2022-03-19 11:15:16 +0800

    fix error

--- libs/util/src/lib/promise.spec.ts
+++ libs/util/src/lib/promise.spec.ts
@@ -5,7 +5,7 @@ describe('promise', () => {
     const t = Date.now();
     await sleep(100);
     const now = Date.now() - t;
-    expect(now).toBeGreaterThanOrEqual(100);
+    expect(now).toBeGreaterThanOrEqual(90);
     expect(now).toBeLessThanOrEqual(200);
   });
 });
