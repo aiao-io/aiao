9a54de24a6428da358fa1c12e5a549b89f0d3b3b
Jimmy Liu <hero63418@gmail.com>
2023-11-22 15:05:56 +0800

    123

--- libs/nest-typeorm-plus/src/lib/utils.ts
+++ libs/nest-typeorm-plus/src/lib/utils.ts
@@ -1,15 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-import { getDataSourceToken, getRepositoryToken } from '@nestjs/typeorm';
-
-import { EntityClassOrSchema } from './interface';
-
-export function getSequelizeRepositoryToken(
-  entity: EntityClassOrSchema,
-  dataSource?: DataSource | DataSourceOptions | string
-): string {
-  return `${getRepositoryToken(entity, dataSource)}_SEQUELIZE_REPOSITORY`;
-}
+import { getDataSourceToken } from '@nestjs/typeorm';
 
 export function getTypeormPlusToken(dataSource?: DataSource | DataSourceOptions | string): string {
   return `${getDataSourceToken(dataSource)}_SEQUELIZE_CONNECTION`;
