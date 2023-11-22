35f45adb60301278bf50347918d0f2f32a089f82
Jimmy Liu <hero63418@gmail.com>
2023-11-22 21:08:24 +0800

    lk2j3

--- apps/dev-nest/src/environments/environment.prod.ts
+++ apps/dev-nest/src/environments/environment.prod.ts
@@ -6,12 +6,7 @@ export const environment: IEnvironment = {
   production: true,
   appEnv: env['APP_ENV'],
   port: +(env['PORT'] || 3000),
-  microservicePort: +(env['MICROSERVICE_PORT'] || 3001),
   imports: [],
   providers: [],
-  controllers: [],
-  aliyun: {
-    key: env['ALIBABA_CLOUD_ACCESS_KEY_ID'] || '',
-    secret: env['ALIBABA_CLOUD_ACCESS_KEY_SECRET'] || ''
-  }
+  controllers: []
 };
--- apps/dev-nest/src/environments/environment.ts
+++ apps/dev-nest/src/environments/environment.ts
@@ -1,18 +1,12 @@
 import { env } from 'process';
 
-import { AppTestController } from '../app/app-test.controller';
 import { IEnvironment } from './interface';
 
 export const environment: IEnvironment = {
   production: false,
   appEnv: env['APP_ENV'] || 'dev',
   port: +(env['PORT'] || 3000),
-  microservicePort: +(env['MICROSERVICE_PORT'] || 3001),
   imports: [],
   providers: [],
-  controllers: [AppTestController],
-  aliyun: {
-    key: env['ALIBABA_CLOUD_ACCESS_KEY_ID'] || 'demo',
-    secret: env['ALIBABA_CLOUD_ACCESS_KEY_SECRET'] || 'demo'
-  }
+  controllers: []
 };
